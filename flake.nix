c4075c29cf292fbc706e4deb9d22aab0c0a1f692
FKouhai <frandres00@gmail.com>
2025-10-05 17:49:50 +0200

    add: helium flake for x86_64-linux architecture

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "A basic AppImage bundler";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        helium = pkgs.appimageTools.wrapType2 rec {
+
+          pname = "helium";
+          version = "0.4.12.1";
+
+          src = pkgs.fetchurl {
+            url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
+            sha256 = "sha256-Za1erduSuuWvfrV/oggSz3ttj79SVV5g1CdXtlWfanU=";
+          };
+
+          extraInstallCommands =
+            let
+              contents = pkgs.appimageTools.extract { inherit pname version src; };
+            in
+            ''
+
+              install -m 444 -D ${contents}/${pname}.desktop -t $out/share/applications
+              substituteInPlace $out/share/applications/${pname}.desktop \
+                --replace 'Exec=AppRun' 'Exec=${pname}'
+              cp -r ${contents}/usr/share/icons $out/share
+            '';
+
+        };
+      in
+      with pkgs;
+      {
+        inherit helium;
+        defaultPackage = helium;
+      }
+    );
+}
