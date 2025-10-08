45aec81aeb2885e4dc27f58c246f23ad0d36ad19
FKouhai <frandres00@gmail.com>
2025-10-08 18:45:01 +0200

    chore: bump helium to 0.5.3.1

--- flake.nix
+++ flake.nix
@@ -26,11 +26,11 @@
         helium = pkgs.appimageTools.wrapType2 rec {
 
           pname = "helium";
-          version = "0.4.12.1";
+          version = "0.5.3.1";
 
           src = pkgs.fetchurl {
             url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-            sha256 = "sha256-Za1erduSuuWvfrV/oggSz3ttj79SVV5g1CdXtlWfanU=";
+            sha256 = "sha256-uPFO1xvL0Uc4eBWZ7yvWoKErUpbyrUDyOMIboOVlzxw=";
           };
 
           extraInstallCommands =
